7154fb36c03344f0be9a54697e9ec8d72933fb46
alexandreohara <alexandre.ohara@hotmail.com>
2018-05-10 10:42:56 -0300

    Added welcome text animation

--- LoginUI/WelcomeViewController.swift
+++ LoginUI/WelcomeViewController.swift
@@ -10,13 +10,31 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
     @IBOutlet weak var userTextField: UILabel!
+    @IBOutlet weak var welcomeTitle: UILabel!
+    @IBOutlet weak var textView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         userTextField.text = UserDefaults.standard.string(forKey: "userName")
+
+
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+            self.textView.transform = CGAffineTransform(translationX: -30, y: 0)
 
+        }) { (_) in
+            
+            UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                self.textView.alpha = 0
+                self.textView.transform = self.textView.transform.translatedBy(x: 0, y: -150)
+            })
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
