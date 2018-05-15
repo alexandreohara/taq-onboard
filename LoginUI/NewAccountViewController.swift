6dd6201f40fd12432176010e15a6f77e28c846d8
alexandreohara <alexandre.ohara@hotmail.com>
2018-05-15 11:21:16 -0300

    Added Create User Integration using Alamofire

--- LoginUI/NewAccountViewController.swift
+++ LoginUI/NewAccountViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class NewAccountViewController: UIViewController {
 
@@ -20,11 +22,15 @@ class NewAccountViewController: UIViewController {
     @IBOutlet weak var confirmPasswordError: UILabel!
     @IBOutlet weak var admSwitch: UISwitch!
     
-    fileprivate func validateTextFields() {
+    var userRole: String = ""
+    
+    fileprivate func validateTextFields() -> Bool {
         let vc: ViewController = ViewController()
+        var isValid: Bool = true
         if (!containOnlyLetters(testStr: nameTextField.text!)) {
             nameTextField.layer.shadowColor = UIColor.red.cgColor
             nameError.isHidden = false
+             isValid = false
         }
         else {
             nameTextField.layer.shadowColor = UIColor.gray.cgColor
@@ -33,6 +39,7 @@ class NewAccountViewController: UIViewController {
         if(!vc.isValidEmail(testStr: emailTextField.text!)) {
             emailTextField.layer.shadowColor = UIColor.red.cgColor
             emailError.isHidden = false
+            isValid = false
         }
             
         else {
@@ -43,6 +50,7 @@ class NewAccountViewController: UIViewController {
         if(passwordTextField.text!.count < 4) {
             passwordTextField.layer.shadowColor = UIColor.red.cgColor
             passwordError.isHidden = false
+            isValid = false
         }
         else {
             passwordTextField.layer.shadowColor = UIColor.gray.cgColor
@@ -51,15 +59,42 @@ class NewAccountViewController: UIViewController {
         if(passwordTextField.text! != confirmPassTextField.text!) {
             confirmPassTextField.layer.shadowColor = UIColor.red.cgColor
             confirmPasswordError.isHidden = false
+            isValid = false
         }
         else {
             confirmPassTextField.layer.shadowColor = UIColor.gray.cgColor
             confirmPasswordError.isHidden = true
         }
+        if(admSwitch.isOn) {
+            userRole = "admin"
+        }
+        else {
+            userRole = "user"
+        }
+        return isValid
+    }
+    
+    func registerNewAccount() {
+        guard let url = URL(string: "https://tq-template-server-sample.herokuapp.com/users") else {return}
+        let header: HTTPHeaders = ["Authorization": UserDefaults.standard.string(forKey: "token")!]
+        let params: [String: Any] = ["name": nameTextField.text!,
+                                     "password": passwordTextField.text!,
+                                     "email": emailTextField.text!,
+                                     "role": userRole]
+        Alamofire.request(url, method: .post, parameters: params, encoding: JSONEncoding.default, headers: header).responseJSON { (response) in
+            guard response.result.isSuccess else {
+                print(response.result.error!)
+                return
+            }
+            let json: JSON = JSON(response.result.value!)
+            print(json)
+        }
     }
     
     @IBAction func registerButton(_ sender: Any) {
-        validateTextFields()
+        if (validateTextFields()) {
+            registerNewAccount()
+        }
     }
     
     func containOnlyLetters(testStr: String) -> Bool {
@@ -77,6 +112,7 @@ class NewAccountViewController: UIViewController {
         emailTextField.setBottomBorder()
         passwordTextField.setBottomBorder()
         confirmPassTextField.setBottomBorder()
+        navigationItem.title = "Create new account"
         // Do any additional setup after loading the view.
     }
 
--- Podfile
+++ Podfile
@@ -1,10 +1,12 @@
-# Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+project 'LoginUI.xcodeproj'
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
 
-target 'Hello World' do
+target 'LoginUI' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for Hello World
-
+  # Pods for LoginUI
+    pod 'Alamofire', '~> 4.7'
+    pod 'SwiftyJSON', '~> 4.0'
 end
